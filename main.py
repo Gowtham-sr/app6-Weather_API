d53dfb616ad9a2d0823a3d834af4b09010ba9cf6
Gowtham-sr <gowtham.s1505@gmail.com>
2023-05-10 09:07:24 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
+# __name__ contains value of __main__ when executed, but when imported it holds the name of script(main.py).
 
-
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
     # flask will look for html documents in templates folder.
 # When app route is called(When user visits), the function is executed.
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
